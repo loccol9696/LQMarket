cd7df5eaeb18acfa663e282ec20cca953e1aaaf7
loccol9696 <loccol9696@gmail.com>
2025-11-09 02:06:48 +0700

    Initial commit

---
+++ src/main/java/org/example/lqmarket/LqMarketApplication.java
@@ -0,0 +1,13 @@
+package org.example.lqmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LqMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LqMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/lqmarket/LqMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lqmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LqMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
